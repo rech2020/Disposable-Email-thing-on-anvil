5d11316b9cbe77b906f95dc5de9d552959d517eb
aleshailin2010@gmail.com <aleshailin2010@gmail.com>
2024-02-15 10:47:33 +0000

    Edited Home

--- client_code/Home.py
+++ client_code/Home.py
@@ -13,14 +13,15 @@ class Home (HomeTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
-    r = get_ip()
-    print(r)
+    self.r = get_ip()
+
+    print(self.r)
     # Any code you write here will run when the form opens.
 
   def button_1_click (self, **event_args):
     # This method is called when the button is clicked
     open_form('Inbox', self.address_box.text + "@disposableemailthingy.anvil.app")
-    print(f"user with ip {r['ip']} has opened email {swld.address_box.text+'@disposableemsilthingy.anvil.app'}")
+    print(f"user with ip {self.r['ip']} has opened email {swld.address_box.text+'@disposableemsilthingy.anvil.app'}")
 
   def address_box_pressed_enter (self, **event_args):
     # This method is called when the user presses Enter in this text box
